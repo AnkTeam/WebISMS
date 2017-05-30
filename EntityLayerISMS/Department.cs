da5d9eaa147f21a8fcd7bd27c266a1554bca5648
vinay <vinayp771@gmail.com>
2017-05-30 10:52:21 +0530

    Added Department Class

---
+++ EntityLayerISMS/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayerISMS
+{
+  public class Department
+    {
+      public int Id { get; set; }
+      public string Name { get; set; }
+    }
+
+  public class DocTemplate
+  {
+      public string DocumentName { get; set; }
+      public string DocumentUrl { get; set; }
+      public int DeptId { get; set; }
+  }
+}
