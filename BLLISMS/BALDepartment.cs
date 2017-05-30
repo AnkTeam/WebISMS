9c6411e973a1de498f0337f5cd22e373442e68a9
vinay <vinayp771@gmail.com>
2017-05-30 10:55:25 +0530

    Added new file in  BAL layer

---
+++ BLLISMS/BALDepartment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayerISMS;
+using DALISMS;
+
+namespace BLLISMS
+{
+    public class BALDepartment
+    {
+
+        public List<Department> GetDepartment(int RoleId)
+        {
+            try
+            {
+                DALDepartment dept = new DALDepartment();
+                return dept.LoadDepartment(RoleId);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+    }
+}
